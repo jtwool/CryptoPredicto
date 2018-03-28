47a106d499e3b78abd429d86e8a2b86c0eb9f18a
John Thomas Wolohan <jwolohan@heron.soic.indiana.edu>
2018-03-28 16:20:35 -0400

    Outline of filesToMatrix

--- filesToMatrix.py
+++ filesToMatrix.py
@@ -1,7 +1,19 @@
 import glob,csv
+from functools import reduce
+
+def linewiseFeatures(a,b):
+  """Adds line--b--to features: a """
+  pass
+
+def daywiseFeatures(d):
+  """Collects summary features for day"""
+  pass
 
 def fileToFeatures(f):
-  
+  """I turn files into feature matricies"""
+  accumulated = reduce(linewiseFeatures,open(f),[]) 
+  m = daywiseFeatures(accumulated)
+  return m
 
 if __name__ == "__main__":
    fs = glob.glob("/scratch/jwolohan/CryptoPredicto/*.txt")
