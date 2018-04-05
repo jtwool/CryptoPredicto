8578d28205bc8427e00806121acb7d1786906a2b
John Thomas Wolohan <jwolohan@silo.soic.indiana.edu>
2018-04-05 11:20:48 -0400

    sentiment analysis added

--- filesToMatrix.py
+++ filesToMatrix.py
@@ -1,35 +1,60 @@
-import glob,csv,json
+import glob,csv,json, numpy as np
+from multiprocessing import Pool
 from functools import reduce
 
-def getSentiment(s):
-  """Get the sentiment score of a string"""
-  pass
+class SentiAnalyzer:
+  def __init__(self):
+    self.lex = {}
+
+  def loadLexicon(self,fp):
+    for line in open(fp):
+      tkn,score,_,_ = line.split()
+      self.lex[tkn]=float(score)
+
+  def lookupWord(self,word):
+    return self.lex.get(word,0.0)
+
+  def analyzeText(self,text):
+    txt = text.lower().split()
+    scores = map(self.lookupWord,txt)
+    return sum(scores)
+
+NRC = SentiAnalyzer()
+NRC.loadLexicon("NRC-unigrams.txt")
 
 def linewiseFeatures(a,b):
   """Adds line--b--to features: a """
   j = json.loads(b)
   txt = j['body']
-  a['textAll'] = a.get('textAll'," ") + " " + txt
+  a['textAll'] = a.get('textAll',' ') + " " + txt
   a['posts'] = a.get('posts',0) + 1
-  a['uusers'] = a.get('uusers',set()) | j['author']
-  a['score'] = a.get('score',[]) + list(j['score'])
-  a['wordcount'] = a.get('wordcount',[]) + list(len(txt.split()))
-  a['sentiment'] = a.get('sentiment',[]) + list(getSentiment(txt))
+  a['uusers'] = a.get('uusers',set()) | set([j['author']])
+  a['score'] = a.get('score',[]) + [j['score']]
+  a['wordcount'] = a.get('wordcount',[]) + [len(txt.split())]
+  a['sentiment'] = a.get('sentiment',[]) + [NRC.analyzeText(txt)]
   return a
 
 def daywiseFeatures(d):
   """Calculates summary features for day, returns matrix"""
-  d['scoreAvg'] = 0
-  d['scoreMax'] = 0
-  d['numUsers'] = len(d['uusers'])
-  d['sentAverage'] = 0 
-  d['sentPositive'] = 0
-  d['sentNegative'] = 0
-  d['sentHi'] = 0
-  d['sentLo'] = 0
-  d['sentNet'] = 0
-  d['sentPosSum'] = 0
-  d['sentNegSum'] = 0
+  d['score'] = d.get('score',[0])
+  d['sentiment'] = d.get('sentiment',[0])
+  d['wordcount'] = d.get('wordcount',[0])
+  d['uusers'] = d.get('uusers',[])
+  d['textAll'] = d.get('textAll',"")
+  d['scoreAvg'] = np.mean(d.get('score'))
+  d['scoreMax'] = np.max(d.get('score'))
+  d['numUsers'] = len(d.get('uusers'))
+  d['sentAverage'] = np.mean(d.get('sentiment'))
+  d['sentPositive'] = len(list(filter(lambda x:x>0,d['sentiment'])))
+  d['sentNegative'] = len(list(filter(lambda x:x<0,d['sentiment'])))
+  d['sentHi'] = np.max(d['sentiment'])
+  d['sentLo'] = np.min(d['sentiment'])
+  d['sentSum'] = sum(d['sentiment'])
+  d['sentPosSum'] = sum(filter(lambda x:x>0, d['sentiment']))
+  d['sentNegSum'] = sum(filter(lambda x:x<0, d['sentiment']))
+  d['wordsAvg'] = np.mean(d['wordcount'])
+  d['wordsTot'] = np.sum(d['wordcount'])
+  d.pop('textAll'); d.pop('wordcount'); d.pop('sentiment'); d.pop('uusers'); d.pop('score')
   return d
 
 def fileToFeatures(f):
@@ -39,7 +64,8 @@ def fileToFeatures(f):
   return m
 
 if __name__ == "__main__":
-   fs = glob.glob("/scratch/jwolohan/CryptoPredicto/*.txt")
-   fs = map(fileToFeatures,fs)
-   with open("/scratch/jwolohan/CrpytoPredicto/Features.csv",'w') as f:
-     csv.DictWriter(f,fieldnames=[])
+   fs = glob.glob("/scratch/jwolohan/CryptoPredicto/data/*.txt")
+   with Pool(processes=24) as p:
+     fs = list(p.map(fileToFeatures,fs))
+   import pandas as pd
+   pd.DataFrame(fs).to_csv("./myFeatures.csv")
