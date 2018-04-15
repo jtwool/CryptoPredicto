ea1267c4f0a99ca5d3c5cce8edd77767d4117cb3
John Thomas Wolohan <jwolohan@silo.soic.indiana.edu>
2018-04-15 17:01:45 -0400

    Not sure... this is a bad update

--- bowAndTypes.py
+++ bowAndTypes.py
@@ -3,6 +3,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from scipy.io import mmwrite 
 from multiprocessing import Pool
 from functools import reduce
+import toolz
+import pandas as pd
 
 def linewiseFeatures(a,b):
   """Adds line--b--to features: a """
@@ -14,22 +16,18 @@ def fileToFeatures(f):
   payload = f.split("/")[-1]
   sub,y,m,d,e = payload.split("-")
   text = reduce(linewiseFeatures,open(f),"") 
-  return {"text":text,"sub":sub,"year":y,
-          "month":m,"day":d,"end":e[:-4]}
+  return {"sub":sub,"year":y,
+          "month":m,"day":d,"end":e[:-4],"text":text}
 
 if __name__ == "__main__":
    fs = glob.glob("/scratch/jwolohan/CryptoPredicto/data/*.txt")
    with Pool(processes=24) as p:
      fs = list(p.map(fileToFeatures,fs))
-   CV = CountVectorizer(max_df=.95, min_df=.01)
-   # Get feature matrix
-   X = CV.fit_transform([f['text'] for f in fs])
-   # Write feature matrix to file
-   mmwrite(a=X,target="myBOW.sparse.matrix.mtx")
-   # Dump Vocab to file (for the future!)
-   json.dump(list(CV.vocabulary_.keys()),open("MyVocab.json",'w'))
-   # Write the file descriptions to a file
-   import pandas as pd
-   df = pd.DataFrame(fs)
-   print(df)
-   df.drop('text',1).to_csv("./myBOWfeatures.csv")
+   gs = toolz.groupby(lambda x:x.get('sub')+x.get('end'),fs)
+   for k,fs in gs.items():
+     CV = CountVectorizer(max_df=.95, min_df=.01)
+     X = CV.fit_transform([f['text'] for f in fs])
+     mmwrite(a=X,target=f"myBOW.sparse.matrix.{k}.mtx")
+     json.dump(list(CV.vocabulary_.keys()),open(f"MyVocab.{k}.json",'w'))
+     df = pd.DataFrame(fs) 
+     df.to_csv(f"./myBOWfeatures.{k}.csv")
